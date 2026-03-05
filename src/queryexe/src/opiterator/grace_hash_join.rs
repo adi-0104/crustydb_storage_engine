582aa791c52b5eb02ff6a733d3d85d05dc2fa5b7
Adithya <adithyasuresh.ak@gmail.com>
2026-03-05 17:12:16 -0600

    add opimplementation for grace_hash

--- src/queryexe/src/opiterator/grace_hash_join.rs
+++ src/queryexe/src/opiterator/grace_hash_join.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap};
 
-use common::{Field, TableSchema, Tuple, ids::{ContainerId, TransactionId}, query::bytecode_expr::ByteCodeExpr, traits::storage_trait::StorageTrait, CrustyError, ids::StateType};
+use common::{CrustyError, Field, TableSchema, Tuple, ids::{ContainerId, Permissions, StateType, TransactionId}, query::bytecode_expr::ByteCodeExpr, traits::storage_trait::StorageTrait};
 use storage::StorageManager;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
@@ -74,4 +74,168 @@ impl GraceHashJoin {
         }
     }
 
+    fn get_partition_id(key: &Field, num_partitions: usize) -> usize {
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        (hasher.finish() as usize) % num_partitions
+    }
+
+    fn load_partition(&mut self, partition_id: usize) {
+        // clear the prev hashmap
+        self.current_join_map.clear();
+
+        // load left partition from disk
+        for (bytes, _) in self.managers.sm.get_iterator(self.left_partition_cids[partition_id], self.tid, Permissions::ReadOnly) {
+            let t = Tuple::from_bytes(&bytes);
+            let key = self.left_expr.eval(&t);
+            self.current_join_map.entry(key).or_insert(Vec::new()).push(t);
+        }
+
+        // setup right partition iterator for probing
+        self.current_right_iter = Some(self.managers.sm.get_iterator(self.right_partition_cids[partition_id], self.tid, Permissions::ReadOnly));
+
+        // find first right tuple
+        self.current_right_tuple = self.current_right_iter.as_mut().and_then(|it| it.next() ).map(|(bytes, _)| Tuple::from_bytes(&bytes));
+
+        self.current_match_idx = 0;
+
+    }
+
 }
+
+impl OpIterator for GraceHashJoin {
+    fn configure(&mut self, will_rewind: bool) {
+        self.left_child.configure(false);
+        self.right_child.configure(will_rewind);
+    }
+
+
+    fn open(&mut self) -> Result<(), CrustyError> {
+        if self.open {
+            return Ok(());
+        }
+        self.right_child.open()?;
+        self.left_child.open()?;
+
+        // create containers to partition left and right table
+        for i in 0..self.num_partitions {
+            let left_cid = GRACE_BASE_CID + i as u16;
+            let right_cid = GRACE_BASE_CID + self.num_partitions as u16 + i as u16;
+
+            // left partitions
+            self.managers.sm.create_container(left_cid, None, StateType::BaseTable, None)?;
+            self.left_partition_cids.push(left_cid);
+            
+            // right
+            self.managers.sm.create_container(right_cid, None, StateType::BaseTable, None)?;
+            self.right_partition_cids.push(right_cid);
+        }
+
+        // hash and partition  left tuple keys to respective containers
+        while let Some(t) = self.left_child.next()? {
+            let key = self.left_expr.eval(&t);
+            let pid = Self::get_partition_id(&key, self.num_partitions);
+            self.managers.sm.insert_value(
+                self.left_partition_cids[pid],
+                t.to_bytes(),
+                self.tid,
+            );
+        }
+
+        // hash and partition  right tuple keys to respective containers
+        while let Some(t) = self.right_child.next()? {
+            let key = self.right_expr.eval(&t);
+            let pid = Self::get_partition_id(&key, self.num_partitions);
+            self.managers.sm.insert_value(
+                self.right_partition_cids[pid],
+                t.to_bytes(),
+                self.tid,
+            );
+        }
+
+        self.open = true;
+
+        Ok(())
+    }
+
+    fn next(&mut self) -> Result<Option<Tuple>, CrustyError> {
+        if !self.open {
+            panic!("Iterator is not open");
+        }
+
+        // load partition from sm into a hash map
+        if !self.partitions_loaded {
+            self.load_partition(0);
+            self.partitions_loaded = true;
+        }
+
+        // loop through partitions
+        while self.current_partition < self.num_partitions {
+            // probe hashtable with each container in the right partition to find matches
+            while let Some(right_tuple) = self.current_right_tuple.clone() {
+                let key = self.right_expr.eval(&right_tuple);
+                if let Some(matches) = self.current_join_map.get(&key) {
+                    if self.current_match_idx < matches.len() {
+                        let left_tuple = matches[self.current_match_idx].clone();
+                        self.current_match_idx += 1;
+                        return Ok(Some(left_tuple.merge(&right_tuple)));
+                    }
+                }
+                // exhausted matches, advance to next right child in partition
+                self.current_right_tuple = self.current_right_iter
+                    .as_mut()
+                    .and_then(|iter| iter.next())
+                    .map(|(bytes, _)| Tuple::from_bytes(&bytes));
+                self.current_match_idx = 0;
+            }
+
+            // advance to next partition
+            self.current_partition += 1;
+            if self.current_partition >= self.num_partitions {
+                return Ok(None);
+            }
+            self.load_partition(self.current_partition);  
+        }
+
+        Ok(None)
+    }
+
+    fn close(&mut self) -> Result<(), CrustyError> {
+        self.left_child.close()?;
+        self.right_child.close()?;
+        self.current_join_map.clear();
+        self.left_partition_cids.clear();
+        self.right_partition_cids.clear();
+        self.current_right_iter = None;
+        self.current_right_tuple = None;
+        self.current_partition = 0;
+        self.current_match_idx = 0;
+        self.partitions_loaded = false;
+        self.open = false;
+        Ok(())
+    }
+    
+    fn rewind(&mut self) -> Result<(), CrustyError> {
+        if !self.open {
+            panic!("Operator has not been opened")
+        }
+        // reset probe state to partition 0
+        self.current_partition = 0;
+        self.current_join_map.clear();
+        self.current_right_iter = None;
+        self.current_right_tuple = None;
+        self.current_match_idx = 0;
+        // force to load partition on return
+        self.partitions_loaded = false;
+        Ok(())
+    }
+
+    fn get_schema(&self) -> &TableSchema {
+        &self.schema
+    }
+
+}
+
+
+
+
